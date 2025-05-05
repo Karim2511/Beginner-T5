46ab05cdf13fd0c63e0a73dfa309ef4e6d5da98f
Karim Mahmoud <kareem2468mahmoud@gmail.com>
2025-05-05 17:49:59 +0300

    Complete Week 5 Tasks

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Beginner_T5
+{
+    #region Fetching simulation
+    //internal class Program
+    //{
+    //    static async Task<string> FetchDataAsync()
+    //    {
+    //        await Task.Delay(2000);
+    //        return "Data fetched from server";
+    //    }
+
+    //    static async Task Main(string[] args)
+    //    {
+    //        Console.WriteLine("Fetching data...");
+    //        string result = await FetchDataAsync();
+    //        Console.WriteLine(result);
+    //    }
+    //}
+    #endregion
+
+    #region Fetching List of Numbers
+    //class Program
+    //{
+    //    static async Task<List<int>> FetchAndFilterEvenNumbersAsync()
+    //    {
+    //        await Task.Delay(1000);
+    //        List<int> numbers = Enumerable.Range(1, 150).ToList(); 
+    //        return numbers.Where(n => n % 2 == 0).ToList();
+    //    }
+    //    static async Task Main(string[] args)
+    //    {
+    //        Console.WriteLine("Fetching and filtering numbers...");
+    //        List<int> evenNumbers = await FetchAndFilterEvenNumbersAsync();
+    //        Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+    //    }
+    //}
+    #endregion
+
+}
